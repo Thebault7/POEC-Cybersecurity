8828e14f49e93bb8dc6a02e03d99e0167cd67f57
Thebault7 <frederic.thebault35@gmail.com>
2021-05-11 11:25:55 +0200

    added hasing and randolGenerator

---
+++ POEC-Cybersecurity/README.md
@@ -0,0 +1,3 @@
+# POEC-Cybersecurity
+Projet de fin de formation "Développeur Cybersécurité" à l'EPSI de Rennes, du 29/03 au 21/06/2021.
+Nom du projet: Bufalo
---
+++ POEC-Cybersecurity/src/test/java/fr/bufalo/acme/AcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bufalo.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
