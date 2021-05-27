c21116ae3e8bc975c9940f01a5e2c984bf6ba4f1
pawsenn <linhchi295@yahoo.fr>
2021-05-28 01:00:20 +0200

    refactor ProductController with constants

--- src/main/java/fr/bufalo/acme/controller/ProductController.java
+++ src/main/java/fr/bufalo/acme/controller/ProductController.java
@@ -1,7 +1,11 @@
 package fr.bufalo.acme.controller;
 
+import fr.bufalo.acme.bo.Category;
 import fr.bufalo.acme.bo.Product;
+import fr.bufalo.acme.bo.Status;
+import fr.bufalo.acme.bo.Vat;
 import fr.bufalo.acme.service.ProductManager;
+import fr.bufalo.acme.utils.reference.ReferenceGeneratorInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -24,39 +28,68 @@ public class ProductController {
 	@Autowired
 	private ProductManager pm;
 
-//	@RequestMapping(path = "/manageProducts", method = RequestMethod.GET)
-//	public void goToManageProducts(ModelMap modelMap) {
-//	}
+	@Autowired
+	private ReferenceGeneratorInterface rg;
 
-	@RequestMapping(path = "/manageProducts", method = RequestMethod.GET)
+	// TODO dans les formulaires distinguer les champs null des champs incorrects/erreur de saisie
+	// TODO signaler les champs required par étoile (html)
+
+	private static final String VAT_PERCENT = "vat_percent";
+	private static final String PRODUCT = "product";
+	private static final String VAT = "vat";
+	private static final String STATUS = "status";
+	private static final String CATEGORY = "category";
+
+	private static final String MANAGE_PRODUCT = "manageProducts";
+	private static final String LIST_PRODUCT = "listProducts";
+	private static final String ADD_PRODUCT = "addProduct";
+	private static final String VIEW_PRODUCT = "viewProduct";
+	private static final String MODIFY_PRODUCT = "modifyProduct";
+
+	private static final String ERROR_MESSAGE = "errorMessage";
+
+
+	@RequestMapping(path = "/" + MANAGE_PRODUCT, method = RequestMethod.GET)
 	public ModelAndView goToManageProducts(ModelMap modelMap) {
 		List<Product> listProducts = pm.findAll();
-		ModelAndView mv = new ModelAndView("manageProducts");
-		mv.addObject("listProducts", listProducts);
+		ModelAndView mv = new ModelAndView(MANAGE_PRODUCT);
+		mv.addObject(LIST_PRODUCT, listProducts);
 		return mv;
 	}
 
-	@RequestMapping(path = "/addProduct", method = RequestMethod.GET)
+	@RequestMapping(path = "/" + ADD_PRODUCT, method = RequestMethod.GET)
 	public ModelAndView goToAddProduct(ModelMap modelMap) {
-		ModelAndView mv = new ModelAndView("addProduct", "product", new Product());
-		mv.addObject("errorMessage", "");
+		ModelAndView mv = new ModelAndView(ADD_PRODUCT, PRODUCT, new Product());
+		mv.addObject(ERROR_MESSAGE, "");
 		return mv;
 	}
 
-	@RequestMapping(path = "/viewProduct", method = RequestMethod.GET)
+	@RequestMapping(path = "/" + VIEW_PRODUCT, method = RequestMethod.GET)
 	public ModelAndView goToViewProduct(ModelMap modelMap, int productId) {
 		Product product = pm.findById(productId);
-		ModelAndView mv = new ModelAndView("viewProduct");
-		mv.addObject("product", product);
+		Vat vat = product.getVat();
+		String vat_percent = floatToPercentageFormat(vat.getPercentage());
+		Status status = product.getStatus();
+		List<Category> categories = product.getCategories();
+		ModelAndView mv = new ModelAndView(VIEW_PRODUCT);
+		mv.addObject(PRODUCT, product);
+		mv.addObject(VAT, vat);
+		mv.addObject(VAT_PERCENT, vat_percent);
+		mv.addObject(STATUS, status);
+		mv.addObject(CATEGORY, categories);
 		return mv;
 	}
 
-	@RequestMapping(path = "/modifyProduct", method = RequestMethod.GET)
+	@RequestMapping(path = "/" + MODIFY_PRODUCT, method = RequestMethod.GET)
 	public ModelAndView goToModifyProduct(ModelMap modelMap, int productId) {
 		Product product = pm.findById(productId);
-		ModelAndView mv = new ModelAndView("modifyProduct");
-		mv.addObject("product", product);
+		ModelAndView mv = new ModelAndView(MODIFY_PRODUCT);
+		mv.addObject(PRODUCT, product);
 		return mv;
+	}
+
+	public String floatToPercentageFormat(float percentage){
+		return String.format("%5.1f%%", percentage*100);
 	}
 
 }
